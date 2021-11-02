375fc63704d4585385bd70da1ec12ded19af2054
Paul Ferlitz <26034195+paulferlitz@users.noreply.github.com>
2021-11-02 16:00:00 +0100

    Offline to online conversion baby

--- src/main/java/me/paulferlitz/Main.java
+++ src/main/java/me/paulferlitz/Main.java
@@ -5,15 +5,16 @@ import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.UUID;
-
-import static java.lang.System.exit;
 
 public class Main
 {
@@ -61,10 +62,10 @@ public class Main
         {
             UUIDHandler uuidHandler = new UUIDHandler();
             JSONArray knownPlayers = FileHandler.loadArrayFromUsercache(pathToWorld + "../usercache.json");
+            Map<UUID, Player> uuidMap = new HashMap<>();
             switch (mode)
             {
                 case "-offline":
-                    Map<UUID, OfflinePlayer> uuidMap = new HashMap<>();
 
                     for (Object obj : knownPlayers)
                     {
@@ -73,7 +74,7 @@ public class Main
                         {
                             uuidMap.put(
                                     UUID.fromString(knownPlayer.getString("uuid")),
-                                    new OfflinePlayer(
+                                    new Player(
                                             knownPlayer.getString("name"),
                                             UUID.fromString(uuidHandler.onlineUUIDToOffline(knownPlayer.getString("uuid")))));
                             System.out.println("Was able to prefetch player " + knownPlayer.getString("name") + " from usercache.json");
@@ -108,7 +109,7 @@ public class Main
                                         {
                                             uuidMap.put(
                                                     UUID.fromString(currentFileName),
-                                                    new OfflinePlayer(
+                                                    new Player(
                                                             uuidHandler.onlineUUIDToName(currentFileName),
                                                             UUID.fromString(uuidHandler.onlineUUIDToOffline(currentFileName))));
                                         }
@@ -120,12 +121,19 @@ public class Main
                                         {
                                             uuidMap.put(
                                                     UUID.fromString(currentFileName),
-                                                    new OfflinePlayer(
+                                                    new Player(
                                                             uuidHandler.onlineUUIDToName(currentFileName),
                                                             UUID.fromString(uuidHandler.onlineUUIDToOffline(currentFileName))));
                                         }
                                         fh.renameFile(pathToWorkingDir, currentFile, uuidMap.get(UUID.fromString(currentFileName)).getUuid() + ".json");
                                     }
+
+                                    Path path = Paths.get(pathToWorld + "../usercache.json");
+                                    Charset charset = StandardCharsets.UTF_8;
+                                    String content = new String(Files.readAllBytes(path), charset);
+                                    content = content.replaceAll(currentFileName, uuidMap.get(UUID.fromString(currentFileName)).getUuid().toString());
+                                    Files.write(path, content.getBytes(charset));
+
                                     System.out.println("Player " + uuidMap.get(UUID.fromString(currentFileName)).getName() + " --> " +
                                             currentFileName + " to " + uuidMap.get(UUID.fromString(currentFileName)).getUuid());
                                 } catch (NullPointerException | IllegalArgumentException | FileAlreadyExistsException e)
@@ -151,10 +159,90 @@ public class Main
                     }
                     break;
                 case "-online":
-                    System.out.println("Converting from offline to online is currently WIP!");
+                    /**
+                     * If you are reading this part of my code: DON'T!!!!!
+                     * You really don't want to see all the copy pasta and the lazy shit I did!
+                     * Just please don't!
+                     * I will fix this later, I promise! Promise made on 02.11.2021 at 4 p.m.!
+                     */
+                    for (Object obj : knownPlayers)
+                    {
+                        JSONObject knownPlayer = (JSONObject) obj;
+                        try
+                        {
+                            uuidMap.put(
+                                    UUID.fromString(knownPlayer.getString("uuid")),
+                                    new Player(
+                                            knownPlayer.getString("name"),
+                                            uuidHandler.onlineNameToUUID(knownPlayer.getString("name"))));
+                            System.out.println("Was able to prefetch player " + knownPlayer.getString("name") + " from usercache.json");
+                        } catch (NullPointerException e)
+                        {
+                            System.out.println("Wasn't able to prefetch player " +
+                                    knownPlayer.getString("name") + " from usercache.json. Probably a pure offline account!");
+                        }
+
+                    }
+
+                    for (String workingDir : workingDirs)
+                    {
+                        System.out.println("Working on " + workingDir + "...");
+                        String pathToWorkingDir = pathToWorld + workingDir + "/";
+                        FileHandler fh = new FileHandler();
+                        File fileList[] = fh.listAllFiles(pathToWorkingDir);
+                        if (fileList == null) continue;
+                        for (File file : fileList)
+                        {
+                            if (file.isFile() && !file.getName().endsWith("old"))
+                            {
+                                String currentFile = file.getName();
+                                try
+                                {
+                                    String currentFileName;
+                                    if (workingDir.equals("playerdata"))
+                                    {
+                                        currentFileName = currentFile.substring(0, currentFile.length() - 4);
+                                        if (uuidMap.containsKey(UUID.fromString(currentFileName)))
+                                        {
+                                            fh.renameFile(pathToWorkingDir, currentFile, uuidMap.get(UUID.fromString(currentFileName)).getUuid() + ".dat");
+                                        }
+                                    } else
+                                    {
+                                        currentFileName = currentFile.substring(0, currentFile.length() - 5);
+                                        if (uuidMap.containsKey(UUID.fromString(currentFileName)))
+                                        {
+                                            fh.renameFile(pathToWorkingDir, currentFile, uuidMap.get(UUID.fromString(currentFileName)).getUuid() + ".json");
+                                        }
+                                    }
+
+                                    Path path = Paths.get(pathToWorld + "../usercache.json");
+                                    Charset charset = StandardCharsets.UTF_8;
+                                    String content = new String(Files.readAllBytes(path), charset);
+                                    content = content.replaceAll(currentFileName, uuidMap.get(UUID.fromString(currentFileName)).getUuid().toString());
+                                    Files.write(path, content.getBytes(charset));
+
+                                    System.out.println("Player " + uuidMap.get(UUID.fromString(currentFileName)).getName() + " --> " +
+                                            currentFileName + " to " + uuidMap.get(UUID.fromString(currentFileName)).getUuid());
+                                } catch (IllegalArgumentException | FileAlreadyExistsException e)
+                                {
+                                    if(e instanceof IllegalArgumentException)
+                                    {
+                                        // For now do nothing
+                                    }else if(e instanceof FileAlreadyExistsException)
+                                    {
+                                        System.out.println("\n!!! FILE CONFLICT !!!");
+                                        System.out.println("Source file: " + pathToWorkingDir + currentFile);
+                                        System.out.println("Output file: " + ((FileAlreadyExistsException) e).getFile());
+                                        System.out.println("Please resolve on your own!\n");
+                                    }
+                                }
+                            }
+
+                        }
+                    }
                     break;
                 default:
-                    System.out.println("There was a fatal error!");
+                    System.out.println("There was a critical error!");
             }
         }
         long endTime = System.nanoTime();
