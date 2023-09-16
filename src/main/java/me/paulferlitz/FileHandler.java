fc3da498e3e39d7a758f8e3dad43038d79fdb9e4
Paul Ferlitz <paul.ferlitz@hotmail.com>
2023-09-16 18:54:56 +0200

    version bump to v2.3.3

--- src/main/java/me/paulferlitz/FileHandler.java
+++ src/main/java/me/paulferlitz/FileHandler.java
@@ -7,7 +7,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.UUID;
 
 /**
  * Class for handling file operations.
