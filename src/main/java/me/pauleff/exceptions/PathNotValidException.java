84e30287544dd18603b17f5a865adb568a199567
Paul Ferlitz <paul.ferlitz@hotmail.com>
2025-05-10 22:59:23 +0200

    Cleaned up PathNotValidException

--- src/main/java/me/pauleff/exceptions/PathNotValidException.java
+++ src/main/java/me/pauleff/exceptions/PathNotValidException.java
@@ -6,13 +6,15 @@ package me.pauleff.exceptions;
  *
  * @author Paul Ferlitz
  */
-public class PathNotValidException extends Exception {
+public class PathNotValidException extends Exception
+{
     /**
      * Constructs a PathNotValidException with a detailed message.
      *
      * @param path The invalid path that caused the exception.
      */
-    public PathNotValidException(String path) {
-        super(String.format("The given path does not exist! Path in question: %s", path));
+    public PathNotValidException(String path)
+    {
+        super(String.format("The given path does not exist: %s", path));
     }
 }
