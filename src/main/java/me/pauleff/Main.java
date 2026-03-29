868177e4044e24229c2a831752c187b5a8ba9ffa
Paul Ferlitz <paul.ferlitz@hotmail.com>
2026-03-30 00:58:12 +0200

    (chore) bump version to 3.1.1

--- src/main/java/me/pauleff/Main.java
+++ src/main/java/me/pauleff/Main.java
@@ -25,7 +25,7 @@ import static java.lang.System.exit;
  */
 public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-    private static final String VERSION = "3.0";
+    private static final String VERSION = "3.1.1";
     private static CommandLine cmd;
     private static HashMap<String, String> serverPropertiesChanges = new HashMap<>();
     private static String mode = "N/A";
