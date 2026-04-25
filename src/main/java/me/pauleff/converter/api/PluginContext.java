5c01951b18e9241b1fadd72eb05788a760f91adc
Paul Ferlitz <paul.ferlitz@hotmail.com>
2026-04-26 00:05:25 +0200

    converted PluginContext back to class

--- src/main/java/me/pauleff/converter/api/PluginContext.java
+++ src/main/java/me/pauleff/converter/api/PluginContext.java
@@ -9,33 +9,42 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
 
-/**
- * Data passed into {@link MOOCPlugin}s. The same instance is passed through the pipeline, so all
- * components are shared by reference—especially {@link #uuidMap()}.
- *
- * @param serverFolder     Absolute server root.
- * @param worldFolder      Absolute world folder ({@code level-name}).
- * @param conversionTarget What target to convert the server to.
- * @param serverType       Detected server type.
- * @param worldFormat      Detected server world worldFormat.
- * @param uuidMap          Map of UUIDs and their converted counterparts.
- */
-public record PluginContext(
-        Path serverFolder,
-        Path worldFolder,
-        ConversionTarget conversionTarget,
-        ServerType serverType,
-        WorldFormat worldFormat,
-        Map<UUID, UUID> uuidMap)
+
+public final class PluginContext
 {
-    public PluginContext
+    private final Path serverFolder;
+    private final Path worldFolder;
+    private final ConversionTarget conversionTarget;
+    private ServerType serverType;
+    private WorldFormat worldFormat;
+    private final Map<UUID, UUID> uuidMap;
+
+    public PluginContext(
+            Path serverFolder,
+            Path worldFolder,
+            ConversionTarget conversionTarget,
+            Map<UUID, UUID> uuidMap)
     {
-        Objects.requireNonNull(serverFolder, "Server folder path can't be null.");
-        Objects.requireNonNull(worldFolder, "World folder path can't be null.");
-        Objects.requireNonNull(conversionTarget, "Target to convert to must be set.");
-        Objects.requireNonNull(serverType, "Server type can't be null.");
-        Objects.requireNonNull(worldFormat, "World worldFormat can't be null.");
-        Objects.requireNonNull(uuidMap, "UUID map can't be null.");
+        this.serverFolder = Objects.requireNonNull(serverFolder, "Server folder path can't be null.");
+        this.worldFolder = Objects.requireNonNull(worldFolder, "World folder path can't be null.");
+        this.conversionTarget = Objects.requireNonNull(conversionTarget, "Target to convert to must be set.");
+        this.uuidMap = Objects.requireNonNull(uuidMap, "UUID map can't be null.");
+    }
+
+    public PluginContext(
+            Path serverFolder,
+            Path worldFolder,
+            ConversionTarget conversionTarget,
+            ServerType serverType,
+            WorldFormat worldFormat,
+            Map<UUID, UUID> uuidMap)
+    {
+        this.serverFolder = Objects.requireNonNull(serverFolder, "Server folder path can't be null.");
+        this.worldFolder = Objects.requireNonNull(worldFolder, "World folder path can't be null.");
+        this.conversionTarget = Objects.requireNonNull(conversionTarget, "Target to convert to must be set.");
+        this.serverType = Objects.requireNonNull(serverType, "Server type can't be null.");
+        this.worldFormat = Objects.requireNonNull(worldFormat, "World worldFormat can't be null.");
+        this.uuidMap = Objects.requireNonNull(uuidMap, "UUID map can't be null.");
     }
 
     /**
@@ -54,5 +63,42 @@ public record PluginContext(
     public UUID getTargetUuid(UUID from)
     {
         return uuidMap.get(Objects.requireNonNull(from, "Original UUID to put into map can't be null."));
+    }
+
+    public Path serverFolder()
+    {
+        return serverFolder;
+    }
+
+    public Path worldFolder()
+    {
+        return worldFolder;
+    }
+
+    public ConversionTarget conversionTarget()
+    {
+        return conversionTarget;
+    }
+
+    public ServerType serverType()
+    {
+        return serverType;
+    }
+
+    public WorldFormat worldFormat() { return worldFormat; }
+
+    public Map<UUID, UUID> uuidMap()
+    {
+        return uuidMap;
+    }
+
+    public void setServerType(ServerType serverType)
+    {
+        this.serverType = serverType;
+    }
+
+    public void setWorldFormat(WorldFormat worldFormat)
+    {
+        this.worldFormat = worldFormat;
     }
 }
--- src/main/java/me/pauleff/converter/plugins/PrefetchUsercache.java
+++ src/main/java/me/pauleff/converter/plugins/PrefetchUsercache.java
@@ -31,11 +31,9 @@ public class PrefetchUsercache implements MOOCPlugin
     }
 
     /**
-     * Paths relative to e.g. {@link PluginContext#serverFolder()} and which will be
-     * the target of this plugin's conversion.
+     * Paths relative to e.g. {@link PluginContext#serverFolder()} and which will be the target of this plugin's conversion.
      *
-     * @param ctx {@link PluginContext} holding information on folders and
-     *            conversion target.
+     * @param ctx {@link PluginContext} holding information on folders and conversion target.
      * @return {@link List} of {@link Path}s to convert; non-null; may be empty
      */
     @Override
@@ -47,10 +45,8 @@ public class PrefetchUsercache implements MOOCPlugin
     /**
      * Do work only on {@code resolvedExistingTargets} (absolute, existing).
      *
-     * @param ctx                     {@link PluginContext} holding information on
-     *                                folders and conversion target.
-     * @param resolvedExistingTargets {@link List} of {@link Path}s to convert or
-     *                                further work on.
+     * @param ctx                     {@link PluginContext} holding information on folders and conversion target.
+     * @param resolvedExistingTargets {@link List} of {@link Path}s to convert or further work on.
      */
     @Override
     public void run(PluginContext ctx, List<Path> resolvedExistingTargets) throws IOException
