f05a3de327f506d95d26bc93dfbbd6d1e79195ee
Paul Ferlitz <paul.ferlitz@hotmail.com>
2026-05-13 02:42:45 +0200

    finished plugins for plugin & modded servers

--- src/main/java/me/pauleff/converter/plugins/ConvertPluginServer.java
+++ src/main/java/me/pauleff/converter/plugins/ConvertPluginServer.java
@@ -1,12 +1,17 @@
 package me.pauleff.converter.plugins;
 
+import me.pauleff.converter.ConverterV3;
+import me.pauleff.converter.WorldFolderStructure;
 import me.pauleff.converter.api.MOOCPlugin;
 import me.pauleff.converter.api.PluginContext;
 import me.pauleff.converter.api.PluginMetadata;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class ConvertPluginServer implements MOOCPlugin
 {
@@ -30,7 +35,35 @@ public class ConvertPluginServer implements MOOCPlugin
     @Override
     public List<Path> setTargets(PluginContext ctx)
     {
-        return List.of();
+        List<Path> worldDimensionRootFolders = new ArrayList<>();
+        worldDimensionRootFolders.add(ctx.worldFolder());
+
+        if (ctx.worldFolderStructure() == WorldFolderStructure.PER_DIMENSION)
+        {
+            Path worldName = ctx.worldFolder().getFileName();
+            worldDimensionRootFolders.add(ctx.serverFolder().resolve(worldName + "_nether"));
+            worldDimensionRootFolders.add(ctx.serverFolder().resolve(worldName + "_the_end"));
+        }
+
+        List<Path> targets = new ArrayList<>();
+        for (Path rootFolder : worldDimensionRootFolders)
+        {
+            if (!Files.exists(rootFolder))
+            {
+                logger().debug("Skipping missing world folder: {}", rootFolder.normalize());
+                continue;
+            }
+            try (Stream<Path> worldFolderStream = Files.walk(rootFolder))
+            {
+                worldFolderStream
+                        .filter(path -> !path.equals(rootFolder))
+                        .forEach(targets::add);
+            } catch (IOException e)
+            {
+                logger().warn("Could not collect Bukkit world targets from {}", rootFolder.normalize(), e);
+            }
+        }
+        return targets;
     }
 
     /**
@@ -42,6 +75,7 @@ public class ConvertPluginServer implements MOOCPlugin
     @Override
     public void run(PluginContext ctx, List<Path> resolvedExistingTargets) throws IOException
     {
-
+        ConverterV3 converterV3 = new ConverterV3(ctx);
+        converterV3.convert(resolvedExistingTargets);
     }
 }
